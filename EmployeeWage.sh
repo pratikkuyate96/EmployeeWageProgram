97592f04d7a84353da4317991d2b756ad328e399
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-03-30 19:39:06 +0530

    ADDED: Employee wage calculate till a condition of total Work...

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,24 +1,31 @@
 #!/bin/bash -x
+
 #CONSTANTS
 EMP_RATE_PER_HR=20
 IS_FULL_TIME=2
 IS_PART_TIME=1
-WORKING_DAYS=20
+MAX_WORKING_DAYS=20
+MAX_WORKING_HOURS=100
 
-for (( i=1; i<=$WORKING_DAYS; i++ ))
+#VARIABLE
+workingDays=0
+workingHours=0
+
+while [[ $workingDays -ne $MAX_WORKING_DAYS && $workingHours -ne $MAX_WORKING_HOURS ]]
 do
+	((workingDays++))
 	randomCheck=$((RANDOM%2+1))
-case $randomCheck in
-	$IS_FULL_TIME)
-		empHours=8
-		;;
-	$IS_PART_TIME)
-		empHours=4
-		;;
-	*)
-		empHours=0
-		;;
-esac
-	salary=$(($empHours * $EMP_RATE_PER_HR))
-	totalSalary=$(($totalSalary + $salary))
+	case $randomCheck in
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	workingHours=$(($workingHours * $empHours))
 done
+totalSalary=$(($workingHours * $EMP_RATE_PER_HR))
