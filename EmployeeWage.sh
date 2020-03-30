e188d3f0e3fb35d47c4c7aae8d2d7aa4321a540d
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-03-30 17:50:00 +0530

    ADDED: Employee wage using use case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -6,13 +6,15 @@ IS_FULL_TIME=2
 IS_PART_TIME=1
 
 randomCheck=$((RANDOM%2+1))
-if [ $randomCheck -eq $IS_FULL_TIME ]
-then
-	empHours=8
-elif [ $randomCheck -eq $IS_PART_TIME ]
-then
-	empHours=4
-else
-	empHours=0
-fi
+case $randomCheck in
+	$IS_FULL_TIME)
+		empHours=8
+		;;
+	$IS_PART_TIME)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
 dailyWageCalculate=$(($empHours * $EMP_RATE_PER_HR))
