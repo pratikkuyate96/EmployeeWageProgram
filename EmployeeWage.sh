8534720eecaa4fe85064ab384cb43493e2b08e7d
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-03-31 23:08:11 +0530

    ADDED: Store daily and total wages

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,7 +4,7 @@
 declare -a dailywage
 
 #CONSTANTS
-EMP_RATE_PER_HR=20
+EMPLOYEE_RATE_PER_HOUR=20
 IS_FULL_TIME=2
 IS_PART_TIME=1
 MAX_WORKING_DAYS=20
@@ -15,7 +15,7 @@ workingDays=0
 workingHours=0
 
 function getWorkingHours() {
-	randomCheck=$((RANDOM%2+1))
+	randomCheck=$((RANDOM%3))
 	case $randomCheck in
 		$IS_FULL_TIME)
 			empHours=8
@@ -33,20 +33,20 @@ function getWorkingHours() {
 #To get daily wage of employee
 function getDailyWage() {
 	wagePerDay=$(($empHours * $EMP_RATE_PER_HR ))
-echo $wagePerDay
+	echo $wagePerDay
 }
 
 #Calculate wage till the days and hours
 function empWageComputation() {
 while [[ $workingDays -ne $MAX_WORKING_DAYS && $workingHours -ne $MAX_WORKING_HOURS ]]
 do
-((workingDays++))
+	((workingDays++))
 	empHours="$(getWorkingHours)"
 	workingHours=$(($workingHours + $empHours))
 	dailyWage[$workingDays]="$(getDailyWage)"
 done
 echo ${dailyWage[@]}
-	totalSalary=$(($workingHours * $EMP_RATE_PER_HR))
+totalSalary=$(($workingHours * $EMPLOYEE_RATE_PER_HOUR))
 }
 
 empWageComputation
