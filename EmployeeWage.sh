62a5df0545fee58f107e04c85a9ecb790f91ade1
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-03-31 18:37:13 +0530

    ADDED: Calculating for month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,24 +1,25 @@
 #!/bin/bash -x
+
 #CONSTANTS
-EMP_RATE_PER_HR=20
+EMPLOYEE_RATE_PER_HOUR=20
 IS_FULL_TIME=2
 IS_PART_TIME=1
 WORKING_DAYS=20
 
-for (( i=1; i<=$WORKING_DAYS; i++ ))
+for (( number=1; number<=$WORKING_DAYS; number++ ))
 do
 	randomCheck=$((RANDOM%2+1))
-case $randomCheck in
-	$IS_FULL_TIME)
-		empHours=8
-		;;
-	$IS_PART_TIME)
-		empHours=4
-		;;
-	*)
-		empHours=0
-		;;
-esac
-	salary=$(($empHours * $EMP_RATE_PER_HR))
+	case $randomCheck in
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	salary=$(($empHours * $EMPLOYEE_RATE_PER_HOUR))
 	totalSalary=$(($totalSalary + $salary))
 done
